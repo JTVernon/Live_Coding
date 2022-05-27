45bc1fb1f9c64e48721520a9dd980645d4dce8c8
John Vernon <j.talmadge.vernon@gmail.com>
2022-05-27 09:13:09 -0400

    Commiting on stranger things day

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import styles from "./styles.css"
+import styles from './styles.css'
 import {useState} from "react"
 
 function App() {
-  const [city, setCity] = useState("Greensboro")
+  const [city, setCity] = useState("")
+  const [status, setStatus] = useState(false);
+
+  //function displayStatus({status}) {
+   // if(status === "pending") {
+   // return ("pending")
+  //} else if (status === "success"){
+   // return ("Success!")
+  //} else if (status === "error") {
+   // return ("Something went wrong :(")
+  //}
+//};
+const params = new URLSearchParams({
+   access_key: "4026473a528afd970328714031324255",
+   query: "Greensboro",
+   units: "f"
+ });
+
+const handleSubmit = async e => {
+  e.preventDefault();
+
+  setStatus(true);
+  console.log(status);
+
+    fetch(`http://api.weatherstack.com/current?${params}`)
+      .then(res => res.json()).then(console.log);
+}
+
+
 
   return (
        <div className="parent-div">
            <div className="one">
                <input
-               name="city"
                type="text"
                id="city"
+               placeholder="Search cities"
                value={city}
-               update={setCity}
                ></input>
            </div>
-           <div className="two">hello</div>
-           <div className="three">hello</div>
+           <div className="two">
+           </div>
+           <button className="three"
+           onClick={handleSubmit}
+           >Submit</button>
        </div>
   )
 };
+
 
 export default App;
