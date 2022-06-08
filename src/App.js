0ae7370365fe5bad9637b10825d3fde3141b79bc
John Vernon <j.talmadge.vernon@gmail.com>
2022-06-08 09:46:02 -0400

    new button created with onchange switching temp units through a math equation.  still working on adding functionality to a dropdown with select.  I can also see some bugs, such as switching a city when not in the default unit used by the API and switching the units with the button.

--- src/App.js
+++ src/App.js
@@ -1,69 +1,115 @@
-import styles from './styles.css'
+import './styles.css'
 import {useState} from "react"
 
 function App() {
   const [city, setCity] = useState("")
+  const [unit, setUnit] = useState("°C");
   const [loading, setLoading] = useState(false);
   const [temp, setTemp] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [windSpeed, setWindSpeed] = useState("");
+  const [precip, setPrecip] = useState("");
+
+  const oppositeUnit = unit === "°C" ? "°F" : "°C";
+
+  const switchUnits = () => {
+    if (unit === "°C") {
+      const newTemp = temp * 1.8 + 32;
+      setTemp(Math.round(newTemp));
+      setUnit(oppositeUnit);
+    }
+
+    if (unit === "°F") {
+      const newTemp = ((temp - 32) * 5) / 9;
+      setTemp(Math.round(newTemp));
+      setUnit(oppositeUnit);
+    }
+  };
 
   //request => current => temperature
 
  const params = new URLSearchParams({
    access_key: "4026473a528afd970328714031324255",
    query: city,
-   units: "f"
+   unit: "c",
  });
 
-const handleSubmit = async e => {
-  e.preventDefault();
-
-  setLoading(true);
-  console.log(loading);
-
-  const url = `http://api.weatherstack.com/current?${params}`
-  
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(response.status);
+ 
+ const handleSubmit = async e => {
+   e.preventDefault();
+   
+   setLoading(true);
+   console.log(loading);
+   
+   const url = `http://api.weatherstack.com/current?${params}`
+   
+   try {
+     const response = await fetch(url);
+     if (!response.ok) {
+       throw new Error(response.status);
+      }
+      const json_data = await response.json();
+      let data = json_data.current;
+      
+      console.log(data);
+      
+      setTemp(data.temperature);
+      setHumidity(data.humidity);
+      setWindSpeed(data.wind_speed);
+      setPrecip(data.precip);
+      
+    } catch (error) {
+      console.log("Error message: ", error);
+    } finally {
+      setLoading(false);
     }
-    const json_data = await response.json();
-    let data = json_data.current;
-
-    console.log(data);
-
-    setTemp(data.temperature)
-
-  } catch (error) {
-    console.log("Error message: ", error);
-  } finally {
-    setLoading(false);
+    console.log(loading);
   }
-  console.log(loading);
- }
-
+  
 
 
   return (
-       <div className="parent-div">
-           <div className="one">
+    <div className="card">
+         <div className="search">
+           <div>
                <input
                type="text"
                id="city"
                placeholder="Search cities"
                value={city}
                onChange={e => setCity(e.target.value)}
-               ></input>
+               />
+           <button className="button" onClick={handleSubmit}>Submit</button>
            </div>
-           <div className="two">
+           <button className="button" onClick={switchUnits}>Convert to {oppositeUnit}</button>
+           
+         </div>
+
+
+
+     <div className="city/temp">
+           <div className="city">
+             <p>Weather in {city}</p>
            </div>
-           <button className="three"
-           onClick={handleSubmit}
-           >Submit</button>
-           <div>
-             The Current Temperature in {city} is {temp} &#8457;
+
+           <div className="temp">
+            <h1>{temp} {unit}</h1>
            </div>
-       </div>
+     </div>
+
+      <div className="description">
+        <div className="rain">
+          Precipitation: {precip}%
+        </div>
+        <div className="humidity">
+          Humidity: {humidity}%
+        </div>
+        <div className = "wind_speed">
+          Wind Speed: {windSpeed}MPH
+        </div>
+      </div>
+
+    </div>   
   )
 };
 
